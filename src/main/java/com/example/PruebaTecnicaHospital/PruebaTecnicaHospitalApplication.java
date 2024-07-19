14737f6b71971deb4ed4f1ce9edcdc079f72366f
DekerWasHere <steban.restrepo@outlook.com>
2024-07-18 20:11:34 -0500

    First Commit

---
+++ src/main/java/com/example/PruebaTecnicaHospital/PruebaTecnicaHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.PruebaTecnicaHospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaHospitalApplication.class, args);
+	}
+
+}
